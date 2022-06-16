d26f532b376a69692a1b420bfe6c54f37052214a
valentinbellini <101220189+valentinbellini@users.noreply.github.com>
2022-06-16 18:25:46 -0300

    Update Botella.cpp

--- TP2_Prueba002/src/Botella.cpp
+++ TP2_Prueba002/src/Botella.cpp
@@ -1,3 +1,10 @@
+/*
+ * Botella.cpp
+ *
+ *  Created on: 16 jun. 2022
+ *      Author: valentin
+ */
+
 #include "Botella.h"
 using namespace std;
 #include <stdlib.h>
@@ -5,13 +12,13 @@ using namespace std;
 #include <iostream>
 
 bool Botella::getTapado(int numero){
-
 	return 0;
 }
 bool Botella::getllenado(int numero){
 	return 0;
 }
-void Botella::logica(){
+
+void Botella::logica(entradas vector[]){
 
 
 }
