d146f65e8ed225358cdd4473ed615de66655a822
valentinbellini <101220189+valentinbellini@users.noreply.github.com>
2022-06-16 18:00:13 -0300

    Update Botella.h

--- TP2_Prueba002/src/Botella.h
+++ TP2_Prueba002/src/Botella.h
@@ -8,10 +8,23 @@
 #ifndef BOTELLA_H_
 #define BOTELLA_H_
 
+
 class Botella {
+private:
+	bool llenado;
+	bool tapado;
+	typedef struct entradas{
+		bool X = 0;
+		bool Y = 0;
+	}entradas;
+
 public:
-	Botella();
+	Botella():llenado(false),tapado(false){}
 	virtual ~Botella();
+	bool getTapado(int); //Responde si una botella esta tapada o no
+	bool getllenado(int); //REsponde si una botella esta llena o no
+	void logica();
+
 };
 
 #endif /* BOTELLA_H_ */
