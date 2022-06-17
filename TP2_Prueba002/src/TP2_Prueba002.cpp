c9847d718a34ed4ef958ec43c75e45b97afd543e
gustyXLE <105175972+gustyXLE@users.noreply.github.com>
2022-06-16 21:51:36 -0300

    Update TP2_Prueba002.cpp

--- TP2_Prueba002/src/TP2_Prueba002.cpp
+++ TP2_Prueba002/src/TP2_Prueba002.cpp
@@ -1,43 +1,74 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 16 jun. 2022
+ *      Author: gusty
+ */
 
 #include <iostream>
 #include <array>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>																 //
+#include <stdbool.h>
+#include <array>
 #include "Botella.h"
-using std::cout;
-using std::cin;
-using std::endl;
 
 #define EVENT_MAX 100
+#define MAX 50																	 //
+#define MIN 1
+
+string Acciones[] = {"Llenando",
+ 	 	 	 	    "Tapando",
+ 	 	 	 	    "Llenando y tapando",
+ 	 	 	 	    "Cinta en movimiento"};
+
+string Eventos[] = {"Imposible",
+				   "Relevante",
+				   "No modifica"};
 
 typedef struct{																	 //
 	char opcion;																 //
 	int n_eventos;																 //
 }Datos_inicio;
 
+typedef struct{
+	bool X_llenado;
+	bool Y_tapado;
+	bool cinta;
+}Proceso;
+
 Datos_inicio *Menu_inicio();
-
-
+void logica(Botella b);
 bool bit_random(double p);
 
+using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::array;
+
 int main(){
-
-	Botella botella;
-
-
 	//int num_simulacion = 0;
+	srand(time(NULL));
 	Datos_inicio *datos_ingreso;
+	int i;
 
 	cout<<"----------------------- Bienvenido!! -----------------------"<<endl;
 	datos_ingreso = Menu_inicio();
 	int cant_eventos = datos_ingreso->n_eventos;
-	entradas *vector[cant_eventos];
-	for(int i = 0; i < cant_eventos; i++){
-		vector[i]->X = bit_random(0.55);
-		vector[i]->Y = bit_random(0.55);
+	Botella botellas[cant_eventos];
+	for(i = 0; i < cant_eventos; i++){
+		botellas[i].setLlena(bit_random(0.55));
+		botellas[i].setTapada(bit_random(0.55));
+		cout<<"Valor de X:"<<botellas[i].getLlena()<<" Valor de Y:"<<botellas[i].getTapada()<<endl;
 	}
 
-	botella.logica(vector[cant_eventos]);
+	for(i = 0;i<cant_eventos;i++){
+		logica(botellas[i]);
+		cout<<"Evento : "<<i<<" "<<botellas[i].getEstado()<<" "<<botellas[i].getEvento()<<endl;
+	}
+
 
 	return 0;
 }
@@ -90,13 +121,35 @@ Datos_inicio* Menu_inicio(){
 	return datos_ingreso;
 }
 
-bool bit_random (double p){														 //
-
-	int valor_random = rand() %2;
+bool bit_random (double p){
+	int valor_random = rand() % MAX + MIN;
 	bool bit;
 
 	if(valor_random > 20/p) bit = true;
 	else bit = false;
 
 	return bit;
+}
+
+void logica(Botella b){
+	bool control = false;
+
+	if(b.getLlena() && b.getTapada() && !control){
+		b.setEstado(Acciones[2]);
+		b.setEvento(Eventos[1]);
+		control = true;
+	}
+	else{
+		if(b.getLlena() && !b.getTapada() && !control){
+			b.setEstado(Acciones[0]);
+			b.setEvento(Eventos[1]);
+		}
+		else{
+			if(!b.getLlena() && !b.getTapada()){
+				b.setEstado(Acciones[3]);
+				b.setEvento(Eventos[1]);
+				control = false;
+			}
+		}
+	}
 }
