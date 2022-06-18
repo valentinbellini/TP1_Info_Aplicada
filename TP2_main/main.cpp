4c9cc5c3fed2603068461e811fb6e382b5ca7935
ivan-saitta <105260203+ivan-saitta@users.noreply.github.com>
2022-06-18 18:00:22 -0300

    Main final

--- TP2_main/main.cpp
+++ TP2_main/main.cpp
@@ -1,99 +1,31 @@
-/*
- * main.cpp
- *
- *  Created on: 16 jun. 2022
- *      Author: gusty
- */
+#include "Controlador.h"
 
-#include <iostream>
+/*#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
-#include "Controlador.h"
-
-#define EVENT_MAX 100
-
-typedef struct{
-	char opcion;
-	int n_eventos;
-}Datos_inicio;
-
-Datos_inicio *Menu_inicio();
 
 using namespace std;
 using std::cout;
 using std::cin;
-using std::endl;
+using std::endl;*/
 
-int main(){
-	int num_simulacion = 0;
-	Datos_inicio *datos_ingreso;
+int main() {
+	int sim = 0;
 
-	cout<<"----------------------- Bienvenido!! -----------------------"<<endl;
-	datos_ingreso = Menu_inicio();
-	while(datos_ingreso->opcion == 'S' || datos_ingreso->opcion == 's'){
-		Controlador driver(datos_ingreso->n_eventos);
-		num_simulacion++;
-		driver.logica(datos_ingreso->n_eventos);
-		cout<<endl;
-		cout<<"//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////"<<endl;
-		cout<<"Resultados de simulacion:"<<num_simulacion<<endl;
-		driver.mostrar(datos_ingreso->n_eventos);
-		cout<<"//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////"<<endl;
-		cout<<endl;
-		driver.~Controlador();
-		datos_ingreso = Menu_inicio();
-	}
-	cout<<"Salio del programa."<<endl;
+		while(1) {
+			sim++;
+			Controlador driver;
+			driver.Menu_inicio(sim);
+
+			if(driver.Get_opcion() == 1) {
+				driver.Set_valores();
+				driver.logica();
+				driver.mostrar(sim);
+			}
+			else break;
+		}
 
 	return 0;
-}
-
-Datos_inicio* Menu_inicio(){
-	Datos_inicio *datos_ingreso;
-	datos_ingreso = new Datos_inicio;
-	if(datos_ingreso == NULL){
-		cout<<"Error!! No se pudo reservar memoria para la variable 'datos_ingreso'"<<endl;
-		return NULL;
-	}
-
-	char opcion;
-	datos_ingreso->n_eventos = 0;
-
-	cout<<"Elige una de las siguientes opciones:"<<endl;
-	cout<<"1 - Simular una nueva partida"<<endl;
-	cout<<"2 - Salir del programa"<<endl;
-	cout<<"Pulsa S para simular nuevamente o F para salir del programa."<<endl;
-	cin>>opcion;
-
-	while((opcion != 'S') && (opcion != 's') && (opcion != 'F') && (opcion != 'f')){
-		cout<<"Debes ingresar el numero de alguna de las opciones del menu."<<endl;
-		cin>>opcion;
-	}
-
-	datos_ingreso->opcion = opcion;
-	cout<<"La opcion elegida es:"<<opcion<<endl;
-
-	if((opcion == 'S') || (opcion == 's')){
-		cout<<"Ingresa la cantidad de eventos que deseas generar: ";
-		cin>>datos_ingreso->n_eventos;
-
-		while((datos_ingreso->n_eventos <= 0) || (datos_ingreso->n_eventos > EVENT_MAX)){
-			if(datos_ingreso->n_eventos <= 0){
-				cout<<"Opcion invalida. La cantidad de eventos a simular debe ser un numero positivo."<<endl;
-				cin>>datos_ingreso->n_eventos;
-			}
-
-			else{
-				if(datos_ingreso->n_eventos > EVENT_MAX){
-					cout<<"La cantidad de eventos ingresada es demasiado grande, por favor ingresa un valor menor a"<<EVENT_MAX<<endl;
-					cin>>datos_ingreso->n_eventos;
-				}
-			}
-		}
-		cout<<"Cantidad de eventos de la simulacion: "<<datos_ingreso->n_eventos<<endl;
-	}
-
-	return datos_ingreso;
 }
